7149705f11fb7fab84827bc82f96e2ce00c6076b
Thomas DE BONA <thomas.debona@gmail.com>
2013-07-29 23:27:00 +0200

    Add presence validations for expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,7 @@
 class Expense < ActiveRecord::Base
+
+  validates :title,          :presence => true
+  validates :operation_date, :presence => true
+  validates :value,          :presence => true
+
 end
