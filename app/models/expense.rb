96745838c088ead40ac9c25918effc1dac2892a8
Thomas DE BONA <thomas.debona@gmail.com>
2013-07-29 23:29:42 +0200

    Define the relation between category and expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,7 @@
 class Expense < ActiveRecord::Base
 
+  belongs_to :category
+
   validates :title,          :presence => true
   validates :operation_date, :presence => true
   validates :value,          :presence => true
