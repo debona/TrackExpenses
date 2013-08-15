f536ea9b2b86fc8fb5f6376da816531a5dd9e3b5
Thomas DE BONA <thomas.debona@gmail.com>
2013-08-15 11:21:14 +0200

    Seed with default banks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+# This file contains all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+## Banks
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bank.create({
+  name: 'CIC',
+  column_separator: ';',
+  date_index: 0,
+  value_index: 2,
+  title_index: 3
+})
+Bank.create({
+  name: 'Société général',
+  column_separator: ';',
+  date_index: 0,
+  value_index: 3,
+  title_index: 2
+})
